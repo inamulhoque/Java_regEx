d5dd7081b1fc56f56bcdb622b18549ed78ae0925
Inamul <inamulhoque097@gmail.com>
2022-06-14 21:27:31 +0530

    Added: Accepting valid email id.

--- src/RegExJava.java
+++ src/RegExJava.java
@@ -3,8 +3,8 @@ import java.util.regex.Pattern;
 
 public class RegExJava {
     public static void main(String[] args) {
-        String name = "Hoque";
-        String regex = "^[A-Z][a-z]{2,}$";
+        String name = "inam.hoq@hotmail.co.in";
+        String regex = "[a-z]\\.?+[a-z]\\.?+@[a-z]+\\.co?+\\.in?";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(name);
         if (matcher.find()){
